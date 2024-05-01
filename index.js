aa8ce8ee3007f061bacd938ed3c2d5652fd124c8
Riaz-Hussain-Saifi <infosaifideveloper@gmail.com>
2024-05-01 21:14:32 +0500

    Updated TO-DO CLI

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-// Importing inquirer module for command-line user interface
+#!/usr/bin/env node
+// Importing required modules
 import inquirer from "inquirer";
+import chalk from "chalk";
 // Array to store tasks
 let app = [];
+// Welcome message
+console.log(chalk.cyanBright.blueBright(`\n\t Welcome to TO-DO app - Code with Saifi Developer`));
+console.log(chalk.yellowBright("*".repeat(66)));
+console.log(`\n`);
 // Async function to use 'await' inside the while loop
-(async () => {
+const main = async () => {
     while (true) {
         // Prompting user for input
         let answers = await inquirer.prompt([
@@ -23,36 +29,48 @@ let app = [];
                 },
             ]);
             let addTask = addTaskAnswer.task;
-            app.push(addTask);
-            console.log(`Task "${addTask}" added successfully.`);
+            app.push(chalk.cyanBright(addTask));
+            console.log(chalk.greenBright(`\n Task "${chalk.cyanBright(addTask)}" added successfully.\n`));
         }
         else if (answers.to_do === "List") {
-            // Listing all tasks
-            console.log("Here is the list of tasks:");
-            app.forEach((task, index) => console.log(`${index + 1}. ${task}`));
-        }
-        else if (answers.to_do === "Delete") {
-            // Deleting a task
-            let indexAnswer = await inquirer.prompt([
-                {
-                    name: "index",
-                    type: "number",
-                    message: "Enter the index of the task to delete: ",
-                },
-            ]);
-            let indexToDelete = indexAnswer.index - 1; // Adjusting index to 0-based
-            if (indexToDelete >= 0 && indexToDelete < app.length) {
-                let deletedTask = app.splice(indexToDelete, 1)[0]; // Removing task from array
-                console.log(`Task "${deletedTask}" deleted successfully.`);
+            // Listing all tasks if available
+            if (app.length === 0) {
+                console.log(chalk.redBright("\n No tasks added yet, please add task in the list \n"));
             }
             else {
-                console.log("Invalid index. No task deleted.");
+                console.log(chalk.greenBright("\n Here is the list of tasks: "));
+                app.forEach((task, index) => console.log(chalk.bold.cyanBright(`${index + 1}. ${task} \n`)));
+            }
+        }
+        else if (answers.to_do === "Delete") {
+            // Deleting a task if tasks are available
+            if (app.length === 0) {
+                console.log(chalk.redBright("\n No tasks added yet, there is nothing to delete.\n"));
+            }
+            else {
+                let indexAnswer = await inquirer.prompt([
+                    {
+                        name: "index",
+                        type: "number",
+                        message: "Enter the index of the task to delete: ",
+                    },
+                ]);
+                let indexToDelete = indexAnswer.index - 1; // Adjusting index to 0-based
+                if (indexToDelete >= 0 && indexToDelete < app.length) {
+                    let deletedTask = app.splice(indexToDelete, 1)[0]; // Removing task from array
+                    console.log(chalk.greenBright(`\n Task ${deletedTask} deleted successfully.\n`));
+                }
+                else {
+                    console.log(chalk.redBright("\t Invalid index. No task deleted. \n"));
+                }
             }
         }
         else if (answers.to_do === "Exit") {
             // Exiting the application
-            console.log(`Thank you for using To-Do application.`);
+            console.log(chalk.yellowBright(`\n\t Thank you for using To-Do application.\n`));
             break;
         }
     }
-})();
+};
+// Calling main function
+main();
